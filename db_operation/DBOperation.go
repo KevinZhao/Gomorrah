7e3b893e9b6be20a08aafc594dccd8c2ed3d4e7f
Kevin Zhao <kevin8093@126.com>
2020-07-05 00:16:00 +0800

    connect to db

---
+++ db_operation/DBOperation.go
@@ -0,0 +1,29 @@
+package db_operation
+
+import (
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+    "fmt"
+    "strings"
+)
+//Db数据库连接池
+var DB *sql.DB
+
+//注意方法名大写，就是public
+func InitDB()  {
+    //构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=utf8"
+    path := strings.Join([]string{userName, ":", password, "@tcp(",ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
+
+    //打开数据库,前者是驱动名，所以要导入： _ "github.com/go-sql-driver/mysql"
+    DB, _ = sql.Open("mysql", path)
+    //设置数据库最大连接数
+    DB.SetConnMaxLifetime(100)
+    //设置上数据库最大闲置连接数
+    DB.SetMaxIdleConns(10)
+    //验证连接
+    if err := DB.Ping(); err != nil{
+        fmt.Println("opon database fail")
+        return
+    }
+    fmt.Println("connnect success")
+}
---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    //"database/sql"
+    //"github.com/go-sql-driver/mysql"
+    //"fmt"
+    //"strings"
+    "db_operation"
+
+)
+
+//注意方法名大写，就是public
+func main()  {
+
+    db_operation.InitDB()
+  
+}
