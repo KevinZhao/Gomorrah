062627b64875e42dbeb1d0c85911680225eb17d2
Zhaokeming <kevin8093@126.com>
2020-07-05 09:38:36 +0800

    folder structure update

--- main/main.go
+++ main/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-    //"database/sql"
-    //"github.com/go-sql-driver/mysql"
-    //"fmt"
-    //"strings"
-    "db_operation"
-
+    "Gomorrah/db_operation"
 )
 
-//注意方法名大写，就是public
 func main()  {
 
     db_operation.InitDB()
